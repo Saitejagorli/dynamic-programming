007ca7573d9066e5420cf1eda06e7806932192b7
saiteja745 <102836362+Saitejagorli@users.noreply.github.com>
2022-11-21 23:50:37 +0530

    Add files via upload

---
+++ longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"Enter the no of array elements:";
+    cin>>n;
+    cout<<"Enter the array elements:";
+    vector<int>v(n);
+    for(i=0;i<n;i++) cin>>v[i];
+    vector<int>lis(n,1);//every element has lis of length 1
+    for(i=1;i<n;i++){
+        for(j=0;j<i;j++){
+            if(v[i]>v[j] && lis[i]<=lis[j])
+                lis[i]=1+lis[j];
+        }
+    }
+    cout<<"Longest increasing subsequence is of length "<<*max_element(lis.begin(),lis.end());
+}
